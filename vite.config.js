7254140085599492a3c1993f17ec74a03324b32c
andrewmartin418 <89543410+andrewmartin418@users.noreply.github.com>
2026-05-26 10:54:19 -0600

    Configure for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/nation-builder/',
   plugins: [react(), tailwindcss()],
 })
